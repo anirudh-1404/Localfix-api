38ec05a92817f114e5fb93613261f8b0fc4f447f
Divyansh <divyanshsharma1972005@gmail.com>
2026-03-24 12:54:04 +0530

    nn

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ import complaintRoutes from "./routes/complaintRoutes.js";
 import problemRoutes from "./routes/problemRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js"
 
-import path from "path";
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
